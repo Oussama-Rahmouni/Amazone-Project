82f8ece54b4623f2b16c38f6db75b86c3ed74e7c
Oussama-Rahmouni <oussama.rahmouni.manager@gmail.com>
2024-07-12 16:14:34 +0100

    DashUser.jsx done

--- client/src/pages/dashboard/DashUsers.jsx
+++ client/src/pages/dashboard/DashUsers.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { rows, columns } from "../homeData";
+import { useNavigate } from "react-router-dom";
 
 const DashUsers = () => {
+  const navigate = useNavigate();
+
+  const handleRowClick = (params) => {
+    // Navigate to the user details page, using the user's id or other identifier
+    navigate(`${params.id}`);
+  };
+
   return (
-    <div
-      style={{
-        margin: "40px",
-        height: 578,
-        width: "90%",
-      }}
-    >
+    <div style={{ margin: "40px", height: 578, width: "90%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
@@ -21,6 +23,7 @@ const DashUsers = () => {
         }}
         pageSizeOptions={[5, 10]}
         checkboxSelection
+        onRowClick={handleRowClick} // Add the row click handler here
       />
     </div>
   );
