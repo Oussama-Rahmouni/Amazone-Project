a6e196195e494146d6b440dba818766ed8ab6600
Oussama-Rahmouni <oussama.rahmouni.manager@gmail.com>
2024-07-12 15:02:43 +0100

    navbar style done

--- client/src/components/dashboard/DasbhoardNavbar.jsx
+++ client/src/components/dashboard/DasbhoardNavbar.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const DasbhoardNavbar = () => {
-  return <div>this is dasbhoard navbar</div>;
+const DashboardNavbar = () => {
+  return (
+    <Navbar bg="light" expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/">
+          Navbar
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarSupportedContent" />
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="me-auto mb-2 mb-lg-0">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/link">
+              <Nav.Link>Link</Nav.Link>
+            </LinkContainer>
+            <NavDropdown title="Dropdown" id="navbarDropdown">
+              <LinkContainer to="/action">
+                <NavDropdown.Item>Action</NavDropdown.Item>
+              </LinkContainer>
+              <LinkContainer to="/another-action">
+                <NavDropdown.Item>Another action</NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to="/something-else">
+                <NavDropdown.Item>Something else here</NavDropdown.Item>
+              </LinkContainer>
+            </NavDropdown>
+          </Nav>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
-export default DasbhoardNavbar;
+export default DashboardNavbar;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,9 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import { ThemeProvider } from "./context/ThemeContext";
 
 const queryClient = new QueryClient();
