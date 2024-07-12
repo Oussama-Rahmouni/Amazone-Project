6961c795c1c2bf363272c25835a0d8d9f1ded765
Oussama-Rahmouni <oussama.rahmouni.manager@gmail.com>
2024-07-12 16:07:34 +0100

    dashUser page done

--- client/src/pages/dashboard/DashUsers.jsx
+++ client/src/pages/dashboard/DashUsers.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { rows, columns } from "../homeData";
 
 const DashUsers = () => {
-  return <div>this is dash users</div>;
+  return (
+    <div
+      style={{
+        margin: "40px",
+        height: 578,
+        width: "90%",
+      }}
+    >
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 9 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
 };
 
 export default DashUsers;
