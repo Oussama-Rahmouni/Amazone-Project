720791fb8feeff9a849b1d03119b3a5cf11e280e
Oussama-Rahmouni <oussama.rahmouni.manager@gmail.com>
2024-07-13 15:15:07 +0100

    DashSChart on work

--- client/src/pages/dashboard/DashSales.jsx
+++ client/src/pages/dashboard/DashSales.jsx
@@ -1,54 +1,20 @@
 import React from "react";
-import { Calendar, dateFnsLocalizer } from "react-big-calendar";
-import format from "date-fns/format";
-import parse from "date-fns/parse";
-import startOfWeek from "date-fns/startOfWeek";
-import getDay from "date-fns/getDay";
-import enUS from "date-fns/locale/en-US";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-
-// Set up localizer by specifying the date-fns config
-const locales = {
-  "en-US": enUS,
-};
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-});
-
-// Sample events for calendar
-const myEventsList = [
-  {
-    title: "Big Meeting",
-    allDay: true,
-    start: new Date(2023, 6, 0),
-    end: new Date(2023, 6, 1),
-  },
-  {
-    title: "Vacation",
-    start: new Date(2023, 6, 7),
-    end: new Date(2023, 6, 10),
-  },
-  {
-    title: "Conference",
-    start: new Date(2023, 6, 20),
-    end: new Date(2023, 6, 23),
-  },
-];
+import StandChart from "../../components/common/StandChart";
+import Chart from "../../components/common/LineChart";
+import "../styles/dashboard.css";
 
 const DashSales = () => {
   return (
-    <div style={{ height: "700px", margin: "50px" }}>
-      <Calendar
-        localizer={localizer}
-        events={myEventsList}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
+    <div className="charts">
+      {/* <div className="chart">
+        <StandChart />
+      </div> */}
+      <div className="chart">
+        <Chart />
+      </div>{" "}
+      <div className="chart">
+        <Chart />
+      </div>
     </div>
   );
 };
